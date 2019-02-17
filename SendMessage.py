4e35b6bdd9ea944985dae70f0cb11bcfb1b710ad
Chris Rech <chrisrech96@gmail.com>
2019-02-17 07:22:30 -0600

    Update SendMessage.py

--- SendMessage.py
+++ SendMessage.py
@@ -1,11 +1,9 @@
-import requests
-import json
+#pip install GroupyAPI
 
-#groupsURL = "https://api.groupme.com/v3/groups?token=yrrudwbqjnb8BbrDqZ6oAhLMfqPfGBaGt5Y97WqV"
-#groupResponse = (requests.get(groupsURL)).json()
+from groupy import Client
+client = Client.from_token("yrrudwbqjnb8BbrDqZ6oAhLMfqPfGBaGt5Y97WqV")
 
-requestURL="https://api.groupme.com/v3/groups/48280673/messages"
-params = {"token": "yrrudwbqjnb8BbrDqZ6oAhLMfqPfGBaGt5Y97WqV"}
-data = {"message": {"text":"hi", "source_guid": "GUID"}}
-messageResponse = (requests.post(requestURL, params=params, json=json.dumps(data)))
-print(messageResponse)
+groups = list(client.groups.list_all())
+
+group = groups[0]
+message = group.post(text='hi')
